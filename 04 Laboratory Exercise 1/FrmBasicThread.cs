5dfda41ff13422a87fd4f6898884ecb6b153fda7
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2022-10-27 15:22:16 +0800

    Add files via upload

---
+++ 04 Laboratory Exercise 1/FrmBasicThread.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beller_BasicThreading
+{
+    public partial class FrmBasicThread : Form
+    {
+        Thread ThreadA, ThreadB;
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            ThreadStart run_this_method = new ThreadStart(MyThreadClass.Thread1);
+            ThreadA = new Thread(run_this_method);
+            ThreadA.Name = "Thread A Process";
+            ThreadB = new Thread(run_this_method);
+            ThreadB.Name = "Thread B Process";
+            Console.WriteLine(lblText.Text);
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadA.Join(); //this will stop a calling thread until the thread terminates.
+            ThreadB.Join();
+            lblText.Text = "-End Of Thread-";
+            Console.WriteLine(lblText.Text);
+        }
+    }
+}
---
+++ 04 Laboratory Exercise 1/MyThreadClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Beller_BasicThreading
+{
+    class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            //this loop will terminate after fifth attempt
+            for (int loopCount = 0; loopCount <= 5; loopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+                Thread.Sleep(1500); //Suspend the current thread for 1.5 seconds.
+            }
+        }
+    }
+}
